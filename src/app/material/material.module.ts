6140fd19d61e20e09c4e31335777c59e911753e4
mhkmcp <mhkmcp@yahoo.com>
2021-12-18 01:27:21 +0600

    added tooltip

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -21,6 +21,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 
@@ -47,6 +48,7 @@ const MaterialComponents = [
   MatRadioModule,
   MatDatepickerModule,
   MatNativeDateModule,
+  MatTooltipModule,
 ]
 
 @NgModule({
